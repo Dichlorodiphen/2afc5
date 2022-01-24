f8b27c10bc6fc2729c8cbcfa7f7ffd83242a8964
David Young <dichlorodiphen@gmail.com>
2022-01-23 22:55:25 -0500

    Textured poles now being rendered

--- main.py
+++ main.py
@@ -4,46 +4,75 @@ import vizshape
 import vizmat
 import os
 
-def generateTerrain():
+def generateTerrainAndObjects():
 	# Sky
 	viz.clearcolor(0, 0.4, 1.0)
 
 	# Make concrete surface
-	WALL_SCALE = [200, 400, 1]
+	WALL_SCALE_CONCRETE = [200, 400, 1]
 	concrete = viz.addTexQuad()
 	concrete.setPosition([-100, 0.148,0])
 	concrete.setEuler([0,90,0])
-	concrete.setScale(WALL_SCALE)
+	concrete.setScale(WALL_SCALE_CONCRETE)
 	
 	# Concrete texture
-	C = 1
-	TEXT_SCALE = [WALL_SCALE[0]/C, WALL_SCALE[1]/C*.8, WALL_SCALE[2]]
+	C_CONCRETE = 1
+	TEXT_SCALE_CONCRETE = [
+		WALL_SCALE_CONCRETE[0]/C_CONCRETE,
+		WALL_SCALE_CONCRETE[1]/C_CONCRETE*.8,
+		WALL_SCALE_CONCRETE[2]
+	]
 	matrix = vizmat.Transform()
-	matrix.setScale(TEXT_SCALE)
-	concrete.texmat(matrix )
-	concreteImage = viz.addTexture('./textures/concrete2.jpg')
-	concreteImage.wrap(viz.WRAP_T, viz.MIRROR)
-	concreteImage.wrap(viz.WRAP_S, viz.REPEAT)
-	concrete.texture(concreteImage)
+	matrix.setScale(TEXT_SCALE_CONCRETE)
+	concrete.texmat(matrix)
+	concrete_texture = viz.addTexture('./textures/concrete2.jpg')
+	concrete_texture.wrap(viz.WRAP_T, viz.MIRROR)
+	concrete_texture.wrap(viz.WRAP_S, viz.REPEAT)
+	concrete.texture(concrete_texture)
 	
 	# Make sand surface
-	WALL_SCALE = [200, 400, 1]
+	WALL_SCALE_SAND = [200, 400, 1]
 	beach = viz.addTexQuad()
 	beach.setPosition( [100, 0.148,0] )
 	beach.setEuler([0,90,0])
-	beach.setScale(WALL_SCALE)
+	beach.setScale(WALL_SCALE_SAND)
 	
 	# Sand texture
-	C = 5
-	TEXT_SCALE = [WALL_SCALE[0]/C, WALL_SCALE[1]/C, WALL_SCALE[2]]
+	C_SAND = 5
+	TEXT_SCALE_SAND = [
+		WALL_SCALE_SAND[0]/C_SAND,
+		WALL_SCALE_SAND[1]/C_SAND,
+		WALL_SCALE_SAND[2]
+	]
 	matrix = vizmat.Transform()
-	matrix.setScale(TEXT_SCALE)
-	beach.texmat( matrix )
+	matrix.setScale(TEXT_SCALE_SAND)
+	beach.texmat(matrix)
 	sand = viz.addTexture('./textures/sand.jpg')
 	sand.wrap(viz.WRAP_T, viz.MIRROR)
 	sand.wrap(viz.WRAP_S, viz.REPEAT)
 	beach.texture(sand)
 	
+	# Generate objects
+	POLE_HEIGHT = 4
+	POLE_RADIUS = 0.25
+	comp_texture = viz.addTexture('./textures/stone.jpg')
+	adj_texture = viz.addTexture('./textures/stone2.jpg')
+	
+	## Comparison pole
+	comp_pole = vizshape.addCylinder(POLE_HEIGHT, POLE_RADIUS)
+	comp_texture.wrap(viz.WRAP_T, viz.REPEAT)
+	comp_texture.wrap(viz.WRAP_S, viz.REPEAT)
+	comp_pole.texture(comp_texture)
+	
+	## Adjustable pole
+	adjustable_pole = vizshape.addCylinder(POLE_HEIGHT, POLE_RADIUS)
+	adj_texture.wrap(viz.WRAP_T, viz.REPEAT)
+	adj_texture.wrap(viz.WRAP_S, viz.REPEAT)
+	adjustable_pole.texture(adj_texture)
+	
+	adjustable_pole.setPosition(2, 1, 5)
+	comp_pole.setPosition(-2, 1, 5)
+
 
 def main():
 	# Start rendering
@@ -70,8 +99,8 @@ def main():
 	file.close()
 	"""
 	
-	# LOAD MODELS/TEXTURES
-	generateTerrain()
+	# Load models / textures
+	generateTerrainAndObjects()
 
 
 if __name__ == "__main__":
