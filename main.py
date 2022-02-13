eda582f12f84c49a014babf8b2d856ab5cf097b6
David Young <dichlorodiphen@gmail.com>
2022-02-12 21:07:20 -0500

    Implement experiment initialization and running minus QUEST+ logic, object movement, data logging, and user interaction

--- main.py
+++ main.py
@@ -2,7 +2,43 @@
 import vizact
 import vizshape
 import vizmat
+import viztask
 import os
+from enum import Enum
+import random
+import time
+
+
+class Terrain(Enum):
+	"""An enum to describe the terrain conditions."""
+	SAND_FIRM = 0
+	SAND_SAND = 1
+	FIRM_FIRM = 2
+	FIRM_SAND = 3
+	
+	
+class Position(Enum):
+	"""An enum to describe the side the standard pole is on."""
+	LEFT = 0
+	RIGHT = 1
+	
+	
+class Experiment:
+	"""A class that encapsulates an experiment and its adaptive staircase."""	
+	def __init__(self, terrain, standard_position, distance, quest):
+		self.terrain = terrain
+		self.standard_position = standard_position
+		self.distance = distance 
+		self.quest = quest
+		
+	
+	def hasNext(self):
+		"""Returns whether or not the current experiment is unfinished."""
+		return True
+		
+		
+	def run(self):
+		pass
 
 def generateTerrainAndObjects():
 	# Sky
@@ -55,23 +91,73 @@ def generateTerrainAndObjects():
 	# Generate objects
 	POLE_HEIGHT = 1.3
 	POLE_RADIUS = 0.35
-	comp_texture = viz.addTexture('./textures/stone.jpg')
-	adj_texture = viz.addTexture('./textures/stone2.jpg')
 	
-	## Comparison pole
-	comp_pole = vizshape.addCylinder(POLE_HEIGHT, POLE_RADIUS)
-	comp_texture.wrap(viz.WRAP_T, viz.REPEAT)
-	comp_texture.wrap(viz.WRAP_S, viz.REPEAT)
-	comp_pole.texture(comp_texture)
+	## Textures
+	blue_texture = viz.addTexture('./textures/blue_granite.jpg')
+	blue_texture.wrap(viz.WRAP_T, viz.REPEAT)
+	blue_texture.wrap(viz.WRAP_S, viz.REPEAT)
+	red_texture = viz.addTexture('./textures/red_granite.jpg')
+	red_texture.wrap(viz.WRAP_T, viz.REPEAT)
+	red_texture.wrap(viz.WRAP_S, viz.REPEAT)
 	
-	## Adjustable pole
-	adjustable_pole = vizshape.addCylinder(POLE_HEIGHT, POLE_RADIUS)
-	adj_texture.wrap(viz.WRAP_T, viz.REPEAT)
-	adj_texture.wrap(viz.WRAP_S, viz.REPEAT)
-	adjustable_pole.texture(adj_texture)
+	## Generate blue standard and comparison poles
+	blue_standard_pole = vizshape.addCylinder(POLE_HEIGHT, POLE_RADIUS)
+	blue_comparison_pole = vizshape.addCylinder(POLE_HEIGHT, POLE_RADIUS)
+	blue_standard_pole.texture(blue_texture)
+	blue_comparison_pole.texture(blue_texture)
 	
-	adjustable_pole.setPosition(2, 1, 5)
-	comp_pole.setPosition(-2, 1, 5)
+	### Generate red pole
+	#adjustable_pole = vizshape.addCylinder(POLE_HEIGHT, POLE_RADIUS)
+	#adjustable_pole.texture(red_texture)
+	
+	## Set pole positions
+	blue_standard_pole.setPosition(2, 1, 5)
+	blue_comparison_pole.setPosition(-2, 1, 5)
+	
+	## Generate star
+	star = viz.add('./models/star/scene.gltf')
+	star.setPosition(0, 10, 20)
+	
+	
+def runExperiments():
+	"""Run each experiment until all are finished."""
+	# Initialize QUEST+
+	
+	# Initialize experiments
+	experiments = []
+	distances = [7, 9, 11]
+	count = 0
+	for i in range(0, 2):
+		for terrain in Terrain:
+			for standard_position in Position:
+				for distance in distances:
+					quest = count ## TODO: change this
+					count += 1
+					current = Experiment(terrain, standard_position, distance, quest)
+					experiments.append(current)
+	
+	done = False
+	while (not done):
+		yield viztask.waitTime(1)
+		# Check if all staircases exhausted
+		done = True
+		for e in experiments:
+			if (e.hasNext()):
+				done = False
+				break
+		if (done):
+			break
+				
+		# Randomly select an experiment
+		# TODO: can we just remove from experiments to improve
+		# runtime?
+		experiment = experiments[random.randint(0, len(experiments) - 1)]
+		while (not experiment.hasNext()):
+			experiment = experiments[random.randint(0, len(experiments) - 1)]
+		
+		
+		# Perform experiment
+		print(experiment.quest)
 
 
 def main():
@@ -101,6 +187,9 @@ def main():
 	
 	# Load models / textures
 	generateTerrainAndObjects()
+	
+	# Proceed until all staircases exhausted
+	experiment_runner = viztask.schedule(runExperiments())
 
 
 if __name__ == "__main__":
